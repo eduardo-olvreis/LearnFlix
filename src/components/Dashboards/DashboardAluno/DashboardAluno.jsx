c92a1c8dacc15e0c73c7e1b5de80c4d3fc13004a
Juaomp <Joaopedromachado830@gmail.com>
2025-11-17 22:14:47 -0300

    Adição de estilização para os componentes da tela de aluno

--- src/components/Dashboards/DashboardAluno/DashboardAluno.jsx
+++ src/components/Dashboards/DashboardAluno/DashboardAluno.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import styles from "./DashboardAluno.module.css";
 
 export default function DashboardAluno() {
   return (
-    <div>
-      <h2>📊 Bem-vindo ao Dashboard do Aluno!</h2>
-      <p>
-        Este é o conteúdo principal que aparece ao acessar a rota base /aluno.
-      </p>
-      <p>
-        Aqui você poderá ver suas notas recentes, próximos prazos e um resumo do
-        seu progresso.
-      </p>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2>📊 Bem-vindo ao Dashboard do Aluno!</h2>
+        <p>
+          Este é o conteúdo principal que aparece ao acessar a rota base /aluno.
+        </p>
+        <p>
+          Aqui você poderá ver suas notas recentes, próximos prazos e um resumo
+          do seu progresso.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Dashboards/NotasAluno/NotasAluno.jsx
+++ src/components/Dashboards/NotasAluno/NotasAluno.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styles from "./NotasAluno.module.css";
 
 export default function NotasAluno() {
   return (
-    <div>
+    <div className={styles.historicoContainer}>
       <h1>Histórico de Notas 📊</h1>
       <p>
         Esta é a **tela de Histórico de Notas** (componente provisório). Aqui
         você poderá consultar todas as suas notas por semestre/disciplina.
       </p>
+      <ul className={styles.notasLista}>
+        <li className={styles.notasItem}>
+          <span>Disciplina: Desenvolvimento Front-End</span>
+          <span className={`${styles.notaDestaque} ${styles.statusAprovado}`}>
+            Nota: 8.5
+          </span>
+        </li>
+        <li className={styles.notasItem}>
+          <span>Disciplina: Desenvolvimento Back-End</span>
+          <span className={`${styles.notaDestaque} ${styles.statusReprovado}`}>
+            Nota: 6.0
+          </span>
+        </li>
+      </ul>
     </div>
   );
 }
