5e6f412531ba0a3165f09f6b6503409786c045ee
eduardo.reis <eduardo.olvreis@gmail.com>
2025-12-11 16:15:07 -0300

    feat: implementação de filtro na aba estudar do aluno

--- src/pages/Aluno/components/ListaRecursos/ListaRecursos.jsx
+++ src/pages/Aluno/components/ListaRecursos/ListaRecursos.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./ListaRecursos.module.css";
+import Input from "../../../../components/Input/Input";
 
 const API_URL = "https://api.sampleapis.com/codingresources/codingResources";
 
@@ -7,6 +8,8 @@ function ListaRecursos() {
   const [recursos, setRecursos] = useState([]);
   const [carregando, setCarregando] = useState(true);
   const [error, setError] = useState(null);
+  
+  const [termoBusca, setTermoBusca] = useState("");
 
   useEffect(() => {
     async function buscarRecursos() {
@@ -32,6 +35,11 @@ function ListaRecursos() {
     buscarRecursos();
   }, []);
 
+  const recursosFiltrados = recursos.filter((recurso) => {
+    const titulo = recurso.title || recurso.description || "";
+    return titulo.toLowerCase().includes(termoBusca.toLowerCase());
+  });
+
   if (carregando) {
     return <div className={styles.loadingMessage}>Carregando recursos...</div>;
   }
@@ -40,43 +48,50 @@ function ListaRecursos() {
     return <div className={styles.errorMessage}>Erro: {error}</div>;
   }
 
-  if (!recursos || recursos.length === 0) {
-    return <div>Nenhum recurso encontrado.</div>;
-  }
-
   return (
     <div className={styles.listaContainer}>
-      <h1>Materiais para estudo: ({recursos.length})</h1>
-      <ul className={styles.recursosLista}>
-        {recursos.map((recurso) => (
-          <li key={recurso.id || recurso.title} className={styles.recursoItem}>
-            <h3>{recurso.title || recurso.description}</h3>
+      <h1>Materiais para estudo</h1>
+      <Input
+        type="text"
+        placeholder="Buscar recurso pelo nome"
+        value={termoBusca}
+        onChange={(e) => setTermoBusca(e.target.value)}
+      />
+      <p className={styles.exibirQuantidade}>Exibindo {recursosFiltrados.length} de {recursos.length} recursos</p>
+      {recursosFiltrados.length === 0 ? (
+        <div>Nenhum recurso encontrado com esse nome.</div>
+      ) : (
+        <ul className={styles.recursosLista}>
+          {recursosFiltrados.map((recurso) => (
+            <li key={recurso.id || recurso.title} className={styles.recursoItem}>
+              <h3>{recurso.title || recurso.description}</h3>
 
-            <p>{recurso.description}</p>
+              <p>{recurso.description}</p>
 
-            <p>
-              <strong>Tipo:</strong> {recurso.types && recurso.types.join(", ")}
-            </p>
+              <p>
+                <strong>Tipo:</strong> {recurso.types && recurso.types.join(", ")}
+              </p>
 
-            {recurso.tags &&
-              Array.isArray(recurso.tags) &&
-              recurso.tags.length > 0 && (
-                <p>
-                  <strong>Tags:</strong> {recurso.tags.join(", ")}
-                </p>
-              )}
+              {recurso.tags &&
+                Array.isArray(recurso.tags) &&
+                recurso.tags.length > 0 && (
+                  <p>
+                    <strong>Tags:</strong> {recurso.tags.join(", ")}
+                  </p>
+                )}
 
-            <a
-              href={recurso.url}
-              className={styles.resourceLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Acessar Recurso
-            </a>
-          </li>
-        ))}
-      </ul>
+              <a
+                href={recurso.url}
+                className={styles.resourceLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Acessar Recurso
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
